60ef734bf4f61590c70e4ebe5ddafa6940c80220
Luan Ramos Vicente <luan.ramos@ferreri.co>
2022-09-13 23:36:53 -0300

    asdf: sets nodejs, yarn and ruby versions to commonly used

---
+++ asdf/setup.sh
@@ -0,0 +1,13 @@
+#!/bin/env sh
+
+echo 'initializing setup for asdf...'
+echo '_____________________________'
+
+echo 'installing asdf...'
+git clone https://github.com/asdf-vm/asdf.git ~/.asdf
+cd ~/.asdf
+git checkout "$(git describe --abbrev=0 --tags)"
+cd
+
+echo 'copying settings file for asdf...'
+ln -sf ~/.dotfiles/asdf/tool-versions ~/.tool-versions
