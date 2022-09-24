61675bbe96ac2b5ef0529f1afcf576be31d27768
Luan Ramos Vicente <luanrvmood@gmail.com>
2022-09-24 20:50:37 -0300

    setup: fixes modules path

--- setup/setup-ubuntu.sh
+++ setup/setup-ubuntu.sh
@@ -15,10 +15,10 @@ sudo apt install -y git
 echo 'downloading dotfiles from remote repository...'
 git clone https://github.com/luanramosvicente/dotfiles.git ~/.dotfiles
 
-. ./git/setup.sh
-. ./bash/setup.sh
-. ./tmux/setup.sh
-. ./vim/setup.sh
-. ./asdf/setup.sh
-. ./ruby/setup.sh
-. ./nodejs/setup.sh
+. ~/.dotfiles/git/setup.sh
+. ~/.dotfiles/bash/setup.sh
+. ~/.dotfiles/tmux/setup.sh
+. ~/.dotfiles/vim/setup.sh
+. ~/.dotfiles/asdf/setup.sh
+. ~/.dotfiles/ruby/setup.sh
+. ~/.dotfiles/nodejs/setup.sh
