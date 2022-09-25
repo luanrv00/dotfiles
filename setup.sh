64337ed8573012e18aa310354ad750a225e9051e
Luan Ramos Vicente <luanrvmood@gmail.com>
2022-09-24 21:00:44 -0300

    setup: moves pre setup process to root setup script

--- setup.sh
+++ setup.sh
@@ -1,3 +1,17 @@
 #!/usr/bin/env sh
 
+echo 'initializing dotfiles setup for Ubuntu...'
+echo '___________________________________________'
+
+echo 'installing git...'
+sudo apt install -y git
+
+echo 'downloading dotfiles from remote repository...'
+git clone https://github.com/luanramosvicente/dotfiles.git ~/.dotfiles
+
+# TODO: temporary pre setup for testing process
+cd ./dotfiles
+git checkout refactor
+cd
+
 . setup/setup-ubuntu.sh
--- setup/setup-ubuntu.sh
+++ setup/setup-ubuntu.sh
@@ -1,24 +1,7 @@
 #!/usr/bin/env sh
 
-echo 'initializing dotfiles setup for Ubuntu...'
-echo '___________________________________________'
-
 echo 'updating apt packages...'
 sudo apt update -y
-
-echo 'installing setup dependencies...'
-echo '________________________________'
-
-echo 'installing git...'
-sudo apt install -y git
-
-echo 'downloading dotfiles from remote repository...'
-git clone https://github.com/luanramosvicente/dotfiles.git ~/.dotfiles
-
-# TODO: temporary pre setup for testing process
-cd ./dotfiles
-git checkout refactor
-cd
 
 . ~/.dotfiles/git/setup.sh
 . ~/.dotfiles/bash/setup.sh
