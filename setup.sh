e26b9da32bf48ce22858feae46823b7d28556538
Luan Ramos Vicente <luanrvmood@gmail.com>
2022-09-24 22:47:51 -0300

    setup: removes temp checkout

--- setup.sh
+++ setup.sh
@@ -10,9 +10,4 @@ sudo apt install -y git
 echo 'downloading dotfiles from remote repository...'
 git clone https://github.com/luanramosvicente/dotfiles.git ~/.dotfiles
 
-# TODO: temporary pre setup for testing process
-cd ~/.dotfiles
-git checkout refactor
-cd
-
 . ~/.dotfiles/setup/setup-ubuntu.sh
