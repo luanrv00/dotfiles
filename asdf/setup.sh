8eec532968df548956c172d52457ea802dd50d66
Luan Ramos Vicente <luanrvmood@gmail.com>
2022-12-30 22:06:56 -0300

    asdf: load asdf after installing

--- asdf/setup.sh
+++ asdf/setup.sh
@@ -9,6 +9,7 @@ git clone https://github.com/asdf-vm/asdf.git ~/.asdf
 cd ~/.asdf
 git checkout "$(git describe --abbrev=0 --tags)"
 cd
+source ~/.bashrc
 
 echo 'copying settings file for asdf...'
 ln -sf ~/.dotfiles/asdf/tool-versions ~/.tool-versions
