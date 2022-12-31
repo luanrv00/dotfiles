5b820dc8ebc597e9e15f77954b2405661960658e
Luan Ramos Vicente <luanrvmood@gmail.com>
2022-12-30 22:25:20 -0300

    docker: adds setup for docker on ubuntu

---
+++ docker/setup.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env sh
+
+echo "\n\n"
+echo 'initializing setup for docker...'
+echo '__________________________________________________________________________'
+
+echo 'installing dependencies...'
+sudo apt install -y ca-certificates curl gnupg lsb-release
+
+echo 'configuring GPG keys...'
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
+echo 'setting up remote repository...'
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+echo 'installing docker...'
+sudo apt install -y docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
