bbcad65241c954779069b4cba4763f014117acb8
Luan Ramos Vicente <luanrvmood@gmail.com>
2022-12-30 21:55:51 -0300

    ruby: installs system dependencies before install ruby

--- ruby/setup.sh
+++ ruby/setup.sh
@@ -4,6 +4,9 @@ echo "\n\n"
 echo 'initializing setup for ruby...'
 echo '__________________________________________________________________________'
 
+echo 'installing dependencies...'
+sudo apt install -y autoconf bison patch build-essential rustc libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libgmp-dev libncurses5-dev libffi-dev libgdbm6 libgdbm-dev libdb-dev uuid-dev
+
 echo 'installing latest version of ruby...'
 asdf plugin add ruby
 asdf install ruby latest
