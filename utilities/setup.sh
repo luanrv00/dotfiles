b9905fbf88a7a6d4097644eb1370372e3e5624e1
Luan Ramos Vicente <luanrvmood@gmail.com>
2023-01-07 01:52:26 -0300

    setup: adds utils setup

---
+++ utilities/setup.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+echo "\n\n"
+echo 'initializing setup for ruby...'
+echo '__________________________________________________________________________'
+
+echo 'installing tmuxinator...'
+gem install tmuxinator
+
