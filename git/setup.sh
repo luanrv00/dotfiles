0a064047096d51e0df7509931d73057475546b81
Luan Ramos Vicente <luanrvmood@gmail.com>
2023-02-03 19:13:28 -0300

    git: installs git

--- git/setup.sh
+++ git/setup.sh
@@ -4,5 +4,8 @@ echo "\n\n"
 echo 'initializing setup for git...'
 echo '__________________________________________________________________________'
 
+echo 'installing git...'
+sudo apt install -y git
+
 echo 'copying settings file for git...'
 ln -sf ~/.dotfiles/git/gitconfig ~/.gitconfig
