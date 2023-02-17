a894a2e8302bf4609373aaad7b0c5f12d3c79ffe
Luan Ramos Vicente <luanrvmood@gmail.com>
2023-02-17 03:06:50 -0300

    chore: removes unused folder

--- utilities/setup.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env sh
-
-echo "\n\n"
-echo 'initializing setup for ruby...'
-echo '__________________________________________________________________________'
-
-echo 'installing tmuxinator...'
-gem install tmuxinator
-
