8bf5a3671cfed27b5cad6d2196248e2e662f542d
luan ramos vicente <luanramosvicente0@gmail.com>
2023-03-06 17:03:22 -0300

    alacritty: adds alacritty config file, links that file

--- alacritty/setup.sh
+++ alacritty/setup.sh
@@ -7,3 +7,6 @@ echo 'installing alacritty...'
 sudo add-apt-repository ppa:aslatter/ppa -y
 sudo apt update -y
 sudo apt install -y alacritty
+
+echo 'copying settings file for alacritty...'
+ln -sf ~/.dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
