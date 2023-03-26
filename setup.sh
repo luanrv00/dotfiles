317a2452571ded90e9093a611528fe933086fc74
luan ramos vicente <luanramosvicente0@gmail.com>
2023-03-26 12:39:56 -0300

    setup: remove setup folder

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@
 # ./setup
 # ./setup [module] (e.g. ./setup vim)
 
-# TODO: fix .dotfiles being removed when running script twice
+# TODO: fix .dotfiles being removed when running script twice before uncomment
 #echo 'installing wget...'
 #sudo apt install -y wget
 #
@@ -35,7 +35,27 @@
 #rmdir ~/dotfiles
 
 if [ $# -eq 0 ]; then
-  . ~/.dotfiles/setup/setup-ubuntu.sh
+  echo 'updating apt packages...'
+  sudo apt update -y
+
+  . ~/.dotfiles/curl/setup.sh
+  . ~/.dotfiles/chrome/setup.sh
+  . ~/.dotfiles/spotify/setup.sh
+  . ~/.dotfiles/dropbox/setup.sh
+  . ~/.dotfiles/telegram/setup.sh
+  . ~/.dotfiles/insomnia/setup.sh
+  . ~/.dotfiles/jetbrains-mono/setup.sh
+  . ~/.dotfiles/git/setup.sh
+  . ~/.dotfiles/bash/setup.sh
+  . ~/.dotfiles/vim/setup.sh
+  . ~/.dotfiles/asdf/setup.sh
+  . ~/.dotfiles/ruby/setup.sh
+  . ~/.dotfiles/nodejs/setup.sh
+  . ~/.dotfiles/python/setup.sh
+  . ~/.dotfiles/tmux/setup.sh
+  . ~/.dotfiles/tmuxinator/setup.sh
+  . ~/.dotfiles/dracula/setup.sh
+  . ~/.dotfiles/alacritty/setup.sh
 else
   . ~/.dotfiles/$1/setup.sh
 fi
