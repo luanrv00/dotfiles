fcc4f96099b9285310f65d147e011d0ee7f6f893
luan ramos vicente <luanramosvicente0@gmail.com>
2023-04-01 22:44:16 -0300

    insomnia: fixes apt package

--- insomnia/setup.sh
+++ insomnia/setup.sh
@@ -5,6 +5,6 @@ echo '__________________________________________________________________________
 
 echo 'installing insomnia...'
 echo "deb [trusted=yes arch=amd64] https://download.konghq.com/insomnia-ubuntu/ default all" \
-      | sudo tee -a /etc/apt/sources.list.d/insomnia.list"]"
+      | sudo tee -a /etc/apt/sources.list.d/insomnia.list
 sudo apt update -y
 sudo apt install -y insomnia
