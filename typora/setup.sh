2cd64cb5abe0eddca144d48438d065897ef0f9dc
luan ramos vicente <luan.ramos@ferreri.co>
2023-05-05 20:34:49 -0300

    typora: adds install script

--- typora/setup.sh
+++ typora/setup.sh
@@ -8,3 +8,9 @@ wget -qO - https://typora.io/linux/public-key.asc | sudo tee /etc/apt/trusted.gp
 sudo add-apt-repository 'deb https://typora.io/linux ./'
 sudo apt update -y
 sudo apt install -y typora
+
+echo 'installing theme...'
+cd
+curl https://github.com/Zabriskije/Typora-GitHub-Themes/archive/master.zip > ~/typora.zip
+unzip ~/typora.zip
+cp ~/Typora-GitHub-Themes-main/github-dark-default.css ~/.config/Typora/themes/
