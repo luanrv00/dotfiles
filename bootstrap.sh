e499275e1ac2b7cdde341d02d19794b7e9d3f480
Luan Ramos Vicente <luanrv00@gmail.com>
2024-10-11 18:36:49 +0000

    render instructions mesage

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,8 @@
-#!/usr/bin/env shchmo
+#!/usr/bin/env sh
 sudo apt install -y git
 git clone https://github.com/luanrv00/dotfiles.git ~/.dotfiles
+echo "--------------------------------------------------------------------"
 echo "Succesfully installed git and downloaded the dotfiles repository!"
 echo "Run ~/.dotfiles/setup.sh for complete setup"
 echo "Run ~/.dotfiles/setup.sh <module> for setup only specified module"
+echo "--------------------------------------------------------------------"
