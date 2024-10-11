09ff353822433b64a0e45a09a1757fe31b234df2
luan ramos vicente <luanrv00@gmail.com>
2024-10-11 17:01:53 -0300

    chore: remove legacy comments

--- setup.sh
+++ setup.sh
@@ -3,50 +3,17 @@
 # script used for setting up a fresh installation of Ubuntu with custom dotfiles
 #
 # Usage:
-#     chmod +x setup.sh
 #     ./setup.sh
-#     ./setup.sh <module> (e.g setup.sh vim)
+#     ./setup.sh <module> (e.g ./setup.sh vim)
 #
-# usage:
-# ./setup
-# ./setup [module] (e.g. ./setup vim)
-
-# TODO: fix .dotfiles being removed when running script twice before uncomment
-#echo 'installing wget...'
-#sudo apt install -y wget
-#
-#echo 'verifying existence of current dotilfes folder...'
-#DOTFILES_PATH = $HOME/.dotfiles
-#if [ -d DOTFILES_PATH ]; then
-#  mv DOTFILES_PATH $HOME/backup-dotfiles
-#fi
-#
-#echo 'downloading dotfiles from remote repository...'
-#wget https://github.com/luanrv/dotfiles/archive/refs/heads/ubuntu.zip -O ~/dotfiles.zip
-#
-#echo 'unziping dotfiles...'
-#sudo apt install -y unzip
-#unzip ~/dotfiles.zip -d ~/dotfiles
-#rm ~/dotfiles.zip
-#
-#echo 'creating folder ~/.dotfiles...'
-#rm -rf ~/.dotfiles # TODO: verifies if existis then save a backup
-#mv ~/dotfiles/* ~/.dotfiles
-#rmdir ~/dotfiles
-
 if [ $# -eq 0 ]; then
   echo 'updating apt packages...'
   sudo apt update -y
 
   . ~/.dotfiles/curl/setup.sh
   . ~/.dotfiles/gpg/setup.sh
-  . ~/.dotfiles/chrome/setup.sh
-  . ~/.dotfiles/spotify/setup.sh
-  . ~/.dotfiles/dropbox/setup.sh
-  . ~/.dotfiles/telegram/setup.sh
-  . ~/.dotfiles/insomnia/setup.sh
-  . ~/.dotfiles/jetbrains-mono/setup.sh
   . ~/.dotfiles/git/setup.sh
+  . ~/.dotfiles/alacritty/setup.sh
   . ~/.dotfiles/bash/setup.sh
   . ~/.dotfiles/vim/setup.sh
   . ~/.dotfiles/asdf/setup.sh
@@ -55,10 +22,15 @@ if [ $# -eq 0 ]; then
   . ~/.dotfiles/python/setup.sh
   . ~/.dotfiles/tmux/setup.sh
   . ~/.dotfiles/tmuxinator/setup.sh
-  #. ~/.dotfiles/dracula/setup.sh
-  . ~/.dotfiles/alacritty/setup.sh
-  . ~/.dotfiles/typora/setup.sh
+  . ~/.dotfiles/spotify/setup.sh
   . ~/.dotfiles/vscode/setup.sh
+  . ~/.dotfiles/telegram/setup.sh
+  . ~/.dotfiles/insomnia/setup.sh
+  . ~/.dotfiles/typora/setup.sh
+  . ~/.dotfiles/jetbrains-mono/setup.sh
+  . ~/.dotfiles/dropbox/setup.sh
+  #. ~/.dotfiles/chrome/setup.sh
+  #. ~/.dotfiles/dracula/setup.sh
 else
   . ~/.dotfiles/$1/setup.sh
 fi
