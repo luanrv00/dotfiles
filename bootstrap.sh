f89fb64a9666592dbc82fbc5c10dfc8cf4c84a1b
Luan Ramos Vicente <luanrv00@gmail.com>
2024-10-11 18:25:53 +0000

    add bootstrap script

---
+++ bootstrap.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env sh
+sudo apt install -y git
+git clone https://github.com/luanrv00/dotfiles.git ~/.dotfiles
+echo "Succesfully installed git and downloaded the dotfiles repository!"
+echo "Run ~/.dotfiles/setup.sh for complete setup"
+echo "Run ~/.dotfiles/setup.sh <module> for setup only specified module"
