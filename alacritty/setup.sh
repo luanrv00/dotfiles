2347c5d906dbd239cc89d0e1185ebcbac2c24290
luan ramos vicente <luanfrv9@gmail.com>
2024-12-05 00:46:40 -0300

    alacritty: fix config file linking

--- alacritty/setup.sh
+++ alacritty/setup.sh
@@ -2,4 +2,5 @@
 sudo add-apt-repository ppa:aslatter/ppa -y
 sudo apt update -y
 sudo apt install -y alacritty
+mkdir -p ~/.config/alacritty
 ln -sf ~/.dotfiles/alacritty/alacritty.yml ~/.config/alacritty/alacritty.yml
